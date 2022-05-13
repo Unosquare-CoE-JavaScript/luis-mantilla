164e4205b6f965250cb2bc34939cdfcf5e2c4998
Luis <luis.mantilla@evertecinc.com>
2022-05-12 23:56:22 -0400

    Add Mongo connection

--- GraphQL_BE/app.js
+++ GraphQL_BE/app.js
@@ -3,15 +3,23 @@ var { graphqlHTTP } = require('express-graphql');
 
 const schema = require('./schema/schema');
 
+const mongoose = require('mongoose');
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true,
 }));
   
-  
-
-app.listen(4000, () => {
-    console.log('Listening port: 4000');
-});
+mongoose.connect(
+    `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.klrp2.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => {
+    app.listen({port: port}, () => {
+        console.log(`Listening port: ${port}`);
+    });
+}).catch((e) => console.log(e));
